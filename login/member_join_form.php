97519b8e57a6685147f1e98fc5c3f81516645458
admin <admin@10.189.210.76>
2018-09-18 17:39:41 +0900

    member

---
+++ login/member_join_form.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<form action="member_join.php" method="post">
+<table>
+	<tr>
+		<td>ID</td>
+		<td><input type="text" name="id"></td>
+	</tr>
+	
+	<tr>
+		<td>PW</td>
+		<td><input type="password" name="pw"></td>
+	</tr>
+	<tr>
+		<td>Name</td>
+		<td><input type="text" name="name"></td>
+	</tr>		
+</table>
+<input type="submit" value="Confirm">
+</form>
+
+</body>
+</html>
